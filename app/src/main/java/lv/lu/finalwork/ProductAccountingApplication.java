d9a6bfa66ba88c5e7662904d4e951d6332fe9743
ValdisMeirens <valdismeirens@gmail.com>
2021-04-18 21:04:09 +0300

    Updated Main method

--- app/src/main/java/lv/lu/finalwork/ProductAccountingApplication.java
+++ app/src/main/java/lv/lu/finalwork/ProductAccountingApplication.java
@@ -8,15 +8,6 @@ public class ProductAccountingApplication {
 
     public static void main(String[] args) {
 
-//        Map<Long, Product> repository = new HashMap<>();
-//        ProductRepository productRepository = new ProductRepository(repository);
-//        ProductMapper productMapper = new ProductMapper();
-//        ProductService service = new ProductService(productRepository, productMapper);
-//        Scanner scanner = new Scanner(System.in);
-//        ConsulUI consulUI = new ConsulUI(service, scanner);
-//
-//        consulUI.startUi();
-
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         ConsulUI consulUI = context.getBean(ConsulUI.class);
         consulUI.startUi();

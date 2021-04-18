15a15cd1517f9cdb3b46bfbcf91c665d60f7399e
ValdisMeirens <valdismeirens@gmail.com>
2021-04-18 20:06:47 +0300

    Added test method display() to be sure, that getBean is working. Changed ProductAccountingApplication to spring style

--- app/src/main/java/lv/lu/finalwork/ProductAccountingApplication.java
+++ app/src/main/java/lv/lu/finalwork/ProductAccountingApplication.java
@@ -5,6 +5,8 @@ import lv.lu.finalwork.repository.ProductRepository;
 import lv.lu.finalwork.service.ProductMapper;
 import lv.lu.finalwork.service.ProductService;
 import lv.lu.finalwork.ui.ConsulUI;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,14 +16,18 @@ public class ProductAccountingApplication {
 
     public static void main(String[] args) {
 
-        Map<Long, Product> repository = new HashMap<>();
-        ProductRepository productRepository = new ProductRepository(repository);
-        ProductMapper productMapper = new ProductMapper();
-        ProductService service = new ProductService(productRepository, productMapper);
-        Scanner scanner = new Scanner(System.in);
-        ConsulUI consulUI = new ConsulUI(service, scanner);
+//        Map<Long, Product> repository = new HashMap<>();
+//        ProductRepository productRepository = new ProductRepository(repository);
+//        ProductMapper productMapper = new ProductMapper();
+//        ProductService service = new ProductService(productRepository, productMapper);
+//        Scanner scanner = new Scanner(System.in);
+//        ConsulUI consulUI = new ConsulUI(service, scanner);
+//
+//        consulUI.startUi();
 
-        consulUI.startUi();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ConsulUI consulUI = context.getBean(ConsulUI.class);
+        consulUI.display();
     }
 
 }
